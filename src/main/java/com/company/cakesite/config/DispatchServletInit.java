be8d830f08cf613f02be1899e8ee559836a5029e
aaranja <fidxle@gmail.com>
2022-04-18 15:45:26 -0700

    Configuration to load views added.

--- src/main/java/com/company/cakesite/config/DispatchServletInit.java
+++ src/main/java/com/company/cakesite/config/DispatchServletInit.java
@@ -6,12 +6,12 @@ public class DispatchServletInit extends AbstractAnnotationConfigDispatcherServl
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class<?>[]{WebApplicationBusinessConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebApplicationContextConfig.class};
+        return new Class<?>[]{WebApplicationContextConfig.class};
     }
 
     @Override
