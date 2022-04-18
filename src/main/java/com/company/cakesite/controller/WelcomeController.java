38a7a3a8e47a8c557698bef4cf27f694e78b5749
aaranja <fidxle@gmail.com>
2022-04-18 15:49:10 -0700

    Enter to welcome view cause a log

--- src/main/java/com/company/cakesite/controller/WelcomeController.java
+++ src/main/java/com/company/cakesite/controller/WelcomeController.java
@@ -1,12 +1,18 @@
 package com.company.cakesite.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-    @RequestMapping("/")
-    public String welcome(){
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(value="/welcome")
+    public String welcome() {
+        logger.info("Returning welcome view");
         return "welcome";
+//        return new ModelAndView("welcome");
     }
 }
---
+++ src/main/resources/welcome.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+<h1>Welcome to the best cake site!</h1>
+</body>
+</html>
