00d361cba399b8b8b9878123d32cdc23aefebc5d
aaranja <fidxle@gmail.com>
2022-04-18 15:46:49 -0700

    Webapp folder localed

--- src/main/java/com/company/cakesite/config/WebApplicationContextConfig.java
+++ src/main/java/com/company/cakesite/config/WebApplicationContextConfig.java
@@ -4,18 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
+@ComponentScan("com.company.*")
 @EnableWebMvc
-@ComponentScan
 public class WebApplicationContextConfig implements WebMvcConfigurer {
     @Bean
     public InternalResourceViewResolver getInternalResourceViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("WEB-INF/jsp/");
+        viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("/")
+//                .setCachePeriod(0);
+//    }
 }
